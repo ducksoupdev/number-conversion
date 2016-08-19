84ec8b292812f469faf64073c147acdb4584216f
levym <ducksoupdev@gmail.com>
2016-08-19 05:19:12 +0100

    Convert single digits

--- src/WordConversion.js
+++ src/WordConversion.js
@@ -2,3 +2,14 @@ function WordConversion() {
     this.value = -1;
     this.text = null;
 }
+
+function _convertNumber(num) {
+    var singleDigitWords = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    return singleDigitWords[num];
+}
+
+WordConversion.prototype.convert = function(num) {
+    this.value = num;
+    this.text = _convertNumber(num);
+    return this.text;
+};
--- src/WordConversionSpec.js
+++ src/WordConversionSpec.js
@@ -1,15 +1,38 @@
-describe("Word conversion", function() {
-  var wordConversion;
+describe("Word conversion", function () {
+    var wordConversion;
 
-  beforeEach(function() {
-    wordConversion = new WordConversion();
-  });
+    beforeEach(function () {
+        wordConversion = new WordConversion();
+    });
 
-  it("should have an empty value", function() {
-      expect(wordConversion.value).toEqual(-1);
-  });
+    it("should have an empty value", function () {
+        expect(wordConversion.value).toEqual(-1);
+    });
 
-  it("should have no text", function() {
-      expect(wordConversion.text).toBeNull();
-  });
+    it("should have no text", function () {
+        expect(wordConversion.text).toBeNull();
+    });
+
+    describe("converting single digits", function () {
+        var expected = {
+            0: "zero",
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+            5: "five",
+            6: "six",
+            7: "seven",
+            8: "eight",
+            9: "nine"
+        };
+
+        for (var num in expected) {
+            if (expected.hasOwnProperty(num)) {
+                it("should convert " + num + " to " + expected[num], function () {
+                    expect(wordConversion.convert(num)).toEqual(expected[num]);
+                });
+            }
+        }
+    });
 });
